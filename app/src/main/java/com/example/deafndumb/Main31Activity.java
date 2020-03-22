f36229c31e4a3e77f9d0168fd3a25a71a94bad23
01Ahmed <ahmedakram018@gmail.com>
2020-03-22 15:55:14 +0500

    abc

--- app/src/main/java/com/example/deafndumb/Main31Activity.java
+++ app/src/main/java/com/example/deafndumb/Main31Activity.java
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main31Activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main31);
+
     }
 }
--- app/src/main/java/com/example/deafndumb/Main32Activity.java
+++ app/src/main/java/com/example/deafndumb/Main32Activity.java
@@ -1,14 +1,19 @@
 package com.example.deafndumb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class Main32Activity extends AppCompatActivity {
 
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main32);
+        setContentView(R.layout.activity_main31);
     }
 }
